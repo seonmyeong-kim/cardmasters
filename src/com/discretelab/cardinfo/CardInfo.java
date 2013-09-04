8c8861e43a6b2c7dec88055a863616b60d5ef7da
discreteb <discreteb@gmail.com>
2013-09-04 16:18:26 +0900

    szD

--- src/com/discretelab/cardinfo/CardInfo.java
+++ src/com/discretelab/cardinfo/CardInfo.java
@@ -3,9 +3,7 @@ package com.discretelab.cardinfo;
 public class CardInfo {
 	public int card_id;
 	public int card_category;
-	
 	public int mana_cost; 
-	
 	public int attackpoint;
 	public int defensepoint;
 	public int lifepoint;
